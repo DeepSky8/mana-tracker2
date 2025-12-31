4a9c231e3039778a04af7fb797f26c0bb2727e9f
Joel Jackson <deepsky8@gmail.com>
2025-12-31 14:40:03 -0500

    Wire up navigation

--- src/app/display/navigation/navigation.ts
+++ src/app/display/navigation/navigation.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, input, Output } from '@angular/core';
 import { defaultGameState, IGameState } from '../../data/library';
 
 @Component({
@@ -9,5 +9,24 @@ import { defaultGameState, IGameState } from '../../data/library';
 })
 export class Navigation {
   state = input<IGameState>(defaultGameState)
+  @Output() untapAll = new EventEmitter();
+  @Output() unpickMana = new EventEmitter();
+  @Output() unsacMana = new EventEmitter();
+  @Output() advanceTurn = new EventEmitter();
 
+  public untapAllMana(): void {
+    this.untapAll.emit();
+  }
+
+  public unpickThisMana(): void {
+    this.unpickMana.emit();
+  }
+
+  public unSacThisMana(): void {
+    this.unsacMana.emit();
+  }
+
+  public nextTurn(): void {
+    this.advanceTurn.emit();
+  }
 }
